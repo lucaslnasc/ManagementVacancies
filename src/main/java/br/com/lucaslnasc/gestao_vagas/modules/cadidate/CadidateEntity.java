efe2c0a93bbf5890be6d5233730e26c41ae0f8d7
lucaslnasc <lucaslnascimento015@gmail.com>
2025-04-10 20:56:55 -0300

    inserindo lib validtion

--- src/main/java/br/com/lucaslnasc/gestao_vagas/modules/cadidate/CadidateEntity.java
+++ src/main/java/br/com/lucaslnasc/gestao_vagas/modules/cadidate/CadidateEntity.java
@@ -2,6 +2,10 @@ package br.com.lucaslnasc.gestao_vagas.modules.cadidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +13,14 @@ public class CadidateEntity {
 
   private UUID id;
   private String name;
+
+  @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não pode estar vazio ou conter apenas espaços")
   private String username;
+
+  @Email(message = "O campo [email] deve ser um email válido")
   private String email;
+
+  @Length(min = 6, max = 20, message = "O campo [password] deve ter entre 6 a 20 caracteres")
   private String password;
   private String description;
   private String curriculum;
--- src/main/java/br/com/lucaslnasc/gestao_vagas/modules/cadidate/controllers/CadidateController.java
+++ src/main/java/br/com/lucaslnasc/gestao_vagas/modules/cadidate/controllers/CadidateController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lucaslnasc.gestao_vagas.modules.cadidate.CadidateEntity;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CadidateController {
 
   @PostMapping("/")
-  public void create(@RequestBody CadidateEntity cadidateEntity) {
+  public void create(@Valid @RequestBody CadidateEntity cadidateEntity) {
     System.out.println("Candidate");
     System.out.println(cadidateEntity.getEmail());
   }
