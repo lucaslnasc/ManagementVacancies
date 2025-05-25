0b11822f02bf01a566a1cb219bde46daf533f5d3
lucaslnasc <lucaslnascimento015@gmail.com>
2025-05-25 20:14:29 -0300

    Atualizações no Docker e no Controller Job

--- src/main/java/br/com/lucaslnasc/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/lucaslnasc/gestao_vagas/modules/company/controllers/JobController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lucaslnasc.gestao_vagas.modules.company.entities.JobEntity;
-import br.com.lucaslnasc.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import br.com.lucaslnasc.gestao_vagas.modules.company.repositories.JobRepository;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,12 +16,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class JobController {
 
   @Autowired
-  private CreateJobUseCase createJobUseCase;
+  private final JobRepository jobRepository;
+
+  JobController(JobRepository jobRepository) {
+    this.jobRepository = jobRepository;
+  }
 
   // @PostMapping("/")
   @PostMapping("/")
   public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
-    return this.createJobUseCase.execute(jobEntity);
+    return this.jobRepository.save(jobEntity);
   }
 
 }
