138fee9b2db40b5976491c41cf6d386efa4245c5
lucaslnasc <lucaslnascimento015@gmail.com>
2025-06-01 19:48:20 -0300

    Finalização de método hash e autentificação da aplicação

--- src/main/java/br/com/lucaslnasc/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/lucaslnasc/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -3,9 +3,13 @@ package br.com.lucaslnasc.gestao_vagas.modules.company.useCases;
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import br.com.lucaslnasc.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.lucaslnasc.gestao_vagas.modules.company.repositories.CompanyRepository;
@@ -13,13 +17,16 @@ import br.com.lucaslnasc.gestao_vagas.modules.company.repositories.CompanyReposi
 @Service
 public class AuthCompanyUseCase {
 
+  @Value("${security.token.secret}")
+  private String secretKey;
+
   @Autowired
   private CompanyRepository companyRepository;
 
   @Autowired
   private PasswordEncoder passwordEncoder;
 
-  public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+  public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
     var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(() -> {
       throw new UsernameNotFoundException("Company not found");
     });
@@ -29,5 +36,11 @@ public class AuthCompanyUseCase {
     if (!passwordMatches) {
       throw new AuthenticationException();
     }
+
+    Algorithm algorithm = Algorithm.HMAC256(secretKey);
+    var token = JWT.create().withIssuer("gestao-vagas")
+        .withSubject(company.getId().toString())
+        .sign(algorithm);
+    return token;
   }
 }
